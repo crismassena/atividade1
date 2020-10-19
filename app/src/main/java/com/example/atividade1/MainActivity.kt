d8b1ade33100b9e56bc8015f4c890ac0161ebe0f
crismassena1 <70332308+crismassena1@users.noreply.github.com>
2020-10-19 14:06:48 -0300

    primeira aula - calculadora

--- app/src/main/java/com/example/atividade1/MainActivity.kt
+++ app/src/main/java/com/example/atividade1/MainActivity.kt
@@ -7,5 +7,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+
+        var idade = 20
+        idade = 30
+
     }
+
+
 }
